ad7ee5d9fa5987ff5221409a0937981b4e8fa008
Born <thiagofborn@gmail.com>
2023-05-24 17:38:16 +0100

    dct PR

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Git!' });
+  res.render('index', { title: 'Hello Git > added to the repository dct!' });
 });
 
 module.exports = router;
